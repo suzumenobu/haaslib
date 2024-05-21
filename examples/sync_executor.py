b097914aaf9f9debe37499860c1a672540f353eb
idevtier <idevtier@gmail.com>
2024-05-21 11:44:25 +0200

    fix(docs)

--- examples/sync_executor.py
+++ examples/sync_executor.py
@@ -1,40 +1,42 @@
+import random
+
 from haaslib import api, lab
 from haaslib.domain import BacktestPeriod
-from haaslib.logger import log
 from haaslib.model import CreateLabRequest
 
 
 def main():
     executor = api.RequestsExecutor(host="127.0.0.1", port=8090, state=api.Guest())
 
-    # Get markets list
     markets = api.get_all_markets(executor)
-    log.info(f"{markets[:5]=}")
+    market = random.choice(markets)
+    print(f"Got {len(markets)} and choosed {market}")
 
     # Authenticate to get access for the all endpoints
     executor = executor.authenticate(email="admin@admin.com", password="adm2inadm4in!")
 
-    # Get accounts list
     accounts = api.get_accounts(executor)
-    log.info(f"{accounts=}")
+    account = random.choice(accounts)
+    print(f"Got {len(accounts)} and choosed {account}")
 
     # Get available scripts
     scripts = api.get_all_scripts(executor)
-    log.info(f"{scripts[:5]=}")
+    script = random.choice(scripts)
+    print(f"Got {len(scripts)} and choosed {script}")
 
     # Create lab
     lab_details = api.create_lab(
         executor,
         CreateLabRequest(
-            script_id=scripts[0].script_id,
+            script_id=script.script_id,
             name="My first lab",
-            account_id=accounts[0].account_id,
-            market=markets[0].as_market_tag(),
+            account_id=account.account_id,
+            market=market.as_market_tag(),
             interval=0,
             default_price_data_style="CandleStick",
         ),
     )
-    log.info(f"{lab_details=}")
+    print(f"{lab_details=}")
 
     # Start lab backtesting & wait for result
     backtesting_result = lab.backtest(
@@ -42,7 +44,7 @@ def main():
         lab_details.lab_id,
         BacktestPeriod(period_type=BacktestPeriod.Type.DAY, count=20),
     )
-    log.info(f"{backtesting_result=}")
+    print(f"{backtesting_result=}")
 
 
 if __name__ == "__main__":
